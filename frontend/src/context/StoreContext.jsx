4d362374fa9fc37239e8bc9861ead39d091a8f43
Sagnik Pal <131534396+SagnikPal-7@users.noreply.github.com>
2025-07-28 02:06:06 +0530

    store

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -16,7 +16,7 @@ const StoreContextProvider = (props) => {
     }
   });
 
-  const url = "https://food-delivery-backend-snowy-one.vercel.app";
+  const url = "https://food-delivery-backend-hjz1.onrender.com";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
