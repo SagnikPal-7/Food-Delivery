9b29e243e3cf66170922b80b7b5f2d38058ba1e4
Sagnik Pal <131534396+SagnikPal-7@users.noreply.github.com>
2025-07-28 01:19:38 +0530

    change3

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,19 @@ app.use(
     credentials: true,
   })
 );
-
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://food-delivery-wine-tau.vercel.app"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
 //cors
 app.options("*", cors());
 //middleware
